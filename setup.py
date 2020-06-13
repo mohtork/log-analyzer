9151942201302c651c05c7a8022b8a4a6e9f0be6
ToRk <mohtork@users.noreply.github.com>
2020-06-14 00:01:22 +0100

    add repo url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 setup(name='LogAnalyzer',
       version='0.1',
       description='Log Analyzer Tool',
-      #url='https://github.com/mohtork/log-analyzer',
+      url='https://github.com/mohtork/log-analyzer',
       author='Torkey',
       author_email='',
       setup_requires='setuptools',
